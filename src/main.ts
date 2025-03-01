c16283fd41b387cdc608212f2e55e8c56b38601c
ene-mona <babaabel09@gmail.com>
2025-03-01 20:39:14 +0100

    removed slash

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,17 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import { ReflectionService } from '@grpc/reflection';
 
+
 async function bootstrap() {
   const PORT = process.env.PORT ?? 3000;  
-  //const GRPC_PORT = process.env.GPRC_PORT ?? 50052  //
-  const GRPC_PORT = PORT
- 
+  const GRPC_PORT = process.env.GPRC_PORT ?? 50052  
+
   const app = await NestFactory.create(AppModule);
   app.enableCors(); 
+  await app.listen(PORT);
   console.log(`✅ REST API is running on http://0.0.0.0:${PORT}`);
 
-  app.connectMicroservice<MicroserviceOptions>( {
+  const grpcApp = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.GRPC,
     options: {
       onLoadPackageDefinition: (pkg, server) => {
@@ -21,13 +22,11 @@ async function bootstrap() {
       },
       package: 'todo',
       protoPath: join(__dirname, '../../proto/todo.proto'),
-      url: `dns:///0.0.0.0:${GRPC_PORT}`,  
+       url: `dns:///0.0.0.0:${GRPC_PORT}`,  
     },
   });
 
-  await app.startAllMicroservices();
-  await app.listen(PORT);
-
+  await grpcApp.listen();
   console.log(`✅ gRPC Service is running on the same port: ${GRPC_PORT}`);
 }
 
