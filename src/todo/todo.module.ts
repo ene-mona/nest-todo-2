87c4f572526d0bc6cb32d9b29275bbfa04873df7
ene-mona <babaabel09@gmail.com>
2025-03-01 15:38:25 +0100

    removed slash

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -17,7 +17,7 @@ import { credentials } from '@grpc/grpc-js';
           package: 'todo',
           protoPath: join(__dirname, '../../../proto/todo.proto'),
           //url: 'localhost:50051', // CHANGE THIS TO YOUR COLLEAGUE'S SERVER
-           url:'dns:///nest-grpc-production-caaf.up.railway.app/:50051',
+           url:'dns:///nest-grpc-production-caaf.up.railway.app:50051',
           // credentials: credentials.createSsl(),
         },
       },
